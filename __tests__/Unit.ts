c430cfbdbb61da67ab1b4eb1c17aa687d77bb31e
Robinlemon <lewis-gibson@hotmail.com>
2019-08-21 20:50:40 +0100

    Fixed Tests + Construction Layout

--- __tests__/Unit.ts
+++ __tests__/Unit.ts
@@ -1,7 +1,26 @@
+import Crypto from 'crypto';
+import MockedEnv from 'mocked-env';
+
+import { SchemaType } from '../src/Schema';
 import TriviaBot from '../src/TriviaBot';
 
 describe('Initialisation', () => {
-    test('Should Initialise Normally', () => {
-        expect(() => new TriviaBot(process.env)).not.toThrow();
+    let Restore: ReturnType<typeof MockedEnv>;
+
+    beforeEach(() => {
+        const MockEnvironment: SchemaType = {
+            ClientID: Crypto.randomBytes(15).toString('hex'),
+            ClientSecret: Crypto.randomBytes(15).toString('hex'),
+            ChannelsList: '',
+        };
+
+        Restore = MockedEnv(MockEnvironment);
+    });
+
+    afterEach(() => Restore());
+
+    test('Normally Constructed', () => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        expect(() => new TriviaBot()).not.toThrow();
     });
 });
