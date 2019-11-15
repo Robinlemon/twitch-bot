2ef8e6463373725e39bfdd25048e9c691f818ca8
Robinlemon <lewis-gibson@hotmail.com>
2019-11-15 01:36:54 +0000

    Adds Feature: !vanish

---
+++ src/Integrations/Vanish/Vanish.services.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+
+/* eslint-disable-next-line */
+import{ Logger } from '@robinlemon/logger';
+
+import MessageQueueDispatcher from '../../Classes/MessageQueueDispatcher';
+import Command, { CommandType } from '../../Decorators/Command';
+import Integration from '../index';
+
+export default class Vanish extends Integration {
+    public constructor(protected ChannelName: string, protected MessageHandler: MessageQueueDispatcher, protected Logger: Logger) {
+        super();
+    }
+
+    public get Identifier(): string {
+        return this.constructor.name;
+    }
+
+    @Command({
+        Identifiers: ['vanish', 'hide', 'disappear'],
+        IncludeProtoNameAsIdentifier: false,
+        Subscriber: false,
+    })
+    public Vanish: CommandType = async (_Context, User): Promise<void> => {
+        this.MessageHandler.Send({
+            Channel: this.ChannelName,
+            Message: `/timeout ${User} 1`,
+        });
+    };
+}
